e463880343582b23ca25e7e7ea6b48f1541ba10c
konebachirou <konebachirou1@gmail.com>
2024-01-17 14:39:54 +0100

    edit algo

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-import numpy as np
-from flask import Flask, jsonify, request
+import random
+from flask import Flask, request
 
 from qlearning import QLearningAgent
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/next_move", methods=["POST"])
+# @app.route("/", methods=["POST"])
 def next_move():
-    request.headers['Content-Type'] = 'application/json'
-    grid = request.get_json()["grid"]
-    ghost_position = request.get_json()["ghost_position"]
-    agent = QLearningAgent(
-        actions=[0, 1, 2, 3],
-        epsilon=0.1,
-        alpha=0.1,
-        gamma=0.9,
-    )
-    agent.update_epsilon(0.999)
-    action = agent.get_next_move(grid, ghost_position)
-    return jsonify({"action": action})
+    """
+    Retourne le prochain déplacement du fantôme en fonction de la position du Pac-Man.
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    Args:
+        data: Un dictionnaire contenant les informations suivantes :
+            * "grid": Le tableau de jeu
+            * "ghost_position": La position du fantôme
+            * "pacman_position": La position de Pac-Man
+
+    Returns:
+        La prochaine position du fantôme.
+    """
+    # data = request.get_json()
+    # grid = data["grid"]
+    # ghost_position = data["ghost_position"]
+    # pacman_position = data["pacman_position"]
+    
+    b = {(0, 0): 0, (0, 1): 0, (0, 2): 0, (0, 3): 0, (0, 4): 0, (0, 5): 0, (1, 0): 0, (1, 1): 3, (1, 2): 0, (1, 3): 0, (1, 4): 0, (1, 5): 0, (2, 0): 0, (2, 1): 0, (2, 2): 0, (2, 3): 0, (2, 4): 0, (2, 5): 0, (3, 0): 0, (3, 1): 0, (3, 2): 0, (3, 3): 0, (3, 4): 3, (3, 5): 0, (4, 0): 0, (4, 1): 0, (4, 2): 0, (4, 3): 0, (4, 4): 0, (4, 5): 0, (5, 0): 0, (5, 1): 0, (5, 2): 0, (5, 3): 0, (5, 4): 0, (5, 5): 0}
+    a =(3,4)
+    c =(1,1)
+        
+    agent = QLearningAgent(
+        ghost_position= c,
+        pacman_position = a,
+        board = b,
+    )
+    
+    q_table = agent.train()
+    
+    next_action = max(q_table)
+    print(next_action)
+    
+next_move()
--- qlearning.py
+++ qlearning.py
@@ -1,56 +1,77 @@
 import numpy as np
+import random
 
+ACTIONS = ['left', 'right', 'up', 'down']
 class QLearningAgent:
+    epsilon=2
+    alpha=0.1
+    gamma=0.9
+    qTable = {} # Maps cell to possible actions. Actions then map to reward
+    
+    def __init__(self,ghost_position, pacman_position, board):
+        self.ghost_position = ghost_position
+        self.pacman_position = pacman_position
+        self.board = board
+        self.initQTable()
 
-    def __init__(self, actions, epsilon, alpha, gamma):
-        if not isinstance(actions, list):
-            raise TypeError("actions must be a list")
+    def initQTable(self):
+        for state in self.board.keys():
+            for action in ["up", "down", "left", "right"]:
+                self.qTable[state, action] = 0
 
-        self.actions = actions
-        self.epsilon = epsilon
-        self.alpha = alpha
-        self.gamma = gamma
-        self.q_table = np.zeros((len(actions), len(actions)))
-        self.memory = []
+    def choose_action(self, state):
+        randInt = random.randint(1,11)
+        if randInt <= 3:
 
-    def get_action(self, state):
-        if np.random.random() < self.epsilon:
-            return np.random.choice(self.actions)
+            validActions = list(filter(lambda action: self.isValidCell(state), ACTIONS))
+            return random.choice(validActions)
         else:
-            return self.argmax_q(state)
+            # Gets all qValues for specified state for all q values
+            # for key, val in self.qTable.items():
+            arr = {key: val for key, val in self.qTable.items() if key[0] == state}
+            
+            # Choisissez l'action avec la plus grande valeur Q (exploitation)
+            var = max(arr)
+            return var[1]
+        
+    def evalQFunction(self,coord, action):
+        new_ghost_position=coord
+        if action == "up":
+            new_ghost_position = (coord[0], int(coord[1]) - 1)
+        elif action == "down":
+            new_ghost_position = (coord[0], coord[1]  + 1)
+        elif action == "left":
+            new_ghost_position = (coord[0] - 1, coord[1] )
+        elif action == "right":
+            new_ghost_position = (coord[0] + 1, coord[1] )
+            
+        reward = self.board[new_ghost_position]
+        maxQSPrime = max([self.qTable[new_ghost_position, action] for action2 in ACTIONS if self.isValidCell(new_ghost_position)])
+        self.qTable[new_ghost_position, action] += (self.alpha * (reward + self.gamma * maxQSPrime - self.qTable[new_ghost_position, action]))
+        
+    def isValidCell(self, coord):
+        xCoord, yCoord = coord[0], coord[1]
+        return (0 <= xCoord < 6  and 0 <= yCoord < 6) 
+    
+    def max_V(dictionary):
+        # Recherche de la clé avec la valeur maximale
+        max_key = None
+        max_value = 0
+        for key, value in dictionary.items():
+            if value > max_value:
+                max_key = key
+                max_value = value
 
-    def update(self, state, action, reward, next_state):
-        self.q_table[state][action] += self.alpha * (reward + self.gamma * self.max_q(next_state) - self.q_table[state][action])
-        self.memory.append((state, action, reward, next_state))
+        # Retour de la clé et de la valeur maximale
+        return max_key, max_value
+    
+    def train(self):
+        for i in range(1000):
+            # Génération d'une action aléatoire
+            action = self.choose_action(self.ghost_position)
 
-    def argmax_q(self, state):
-        return np.argmax(self.q_table[state])
+            # Mise à jour de la fonction Q
+            self.evalQFunction(self.ghost_position, action)
 
-    def max_q(self, state):
-        return np.max(self.q_table[state])
-
-    def get_next_move(self, grid, ghost_position):
-        state = self.get_state(grid, ghost_position)
-        action = self.get_action(state)
-        return action
-
-    def get_state(self, grid, ghost_position):
-        state = np.zeros(len(grid))
-        state[ghost_position] = 1
-        return state
-
-    def update_epsilon(self, decay):
-        self.epsilon *= decay
-
-    def explore_randomly(self, probability):
-        if np.random.random() < probability:
-            return True
-        else:
-            return False
-
-    def remember(self, state, action, reward, next_state):
-        self.memory.append((state, action, reward, next_state))
-
-    def replay(self):
-        for state, action, reward, next_state in self.memory:
-            self.update(state, action, reward, next_state)
+        return self.qTable
+       
